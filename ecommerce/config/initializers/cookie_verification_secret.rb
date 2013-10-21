5dc94bb778772198c83aed0c4c0dee7ca6a46eca
Graham Tucker <gttu222@cs.uky.edu>
2013-10-21 14:16:42 -0400

    this is the ruby files

---
+++ ecommerce/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '5efc6412be7ac0a36cb5ac5526a8f9e0f77415183160b7b7445df18f49d76095322974b42f3df86ff71dbc12499574d7159fdcba6230ed153dd016da55026e5f';
